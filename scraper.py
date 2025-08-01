81eb3749c6cba2d1848688bd7a4d2f4da8dc24ee
DADGE RUTUJA MAROTI <dadge.231478@srttc.ac.in>
2025-08-02 02:59:33 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,8 +11,6 @@ def scrape_case_details(case_type, case_number, filing_year):
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
