2d9d281436e9e49c11e945461c70b0ea820352b8
DADGE RUTUJA MAROTI <dadge.231478@srttc.ac.in>
2025-08-02 02:58:09 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# Install dependencies for Chrome
+# Install Chrome for Selenium
 apt-get update
-apt-get install -y wget curl unzip gnupg
-
-# Download and install Chrome
+apt-get install -y wget unzip curl
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
