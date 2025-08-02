9ff76625fe03da70958832295225406f626a140a
DADGE RUTUJA MAROTI <dadge.231478@srttc.ac.in>
2025-08-02 15:20:05 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y wget unzip curl
+apt-get install -y wget unzip curl gnupg
 
-# Install Chrome
+# Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Verify install
-google-chrome --version
+# Install ChromeDriver
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
